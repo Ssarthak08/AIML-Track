8f04eff5743d0574b9d184da1d5926fef80ecb45
Sarthak Gulati <gulatisarthak2003@gmail.com>
2026-02-02 14:56:07 +0530

    Pydantic

---
+++ 01_Python/04_Pydantic /main.py
@@ -0,0 +1,22 @@
+# pydantic is used to eliminate type validation. expected input format = expected input's format 
+from pydantic import BaseModel # type: ignore
+from typing import List,Dict,Optional
+
+class Patient(BaseModel):
+    name: str
+    age: int
+    weight : float
+    married : Optional[bool] = None
+    allergies : List[str]
+    contact_details : Dict[str, str]
+
+def insert_patient_data(patient : Patient):   # () type validation, param --> str, hoker ayegi
+    print(patient.age)
+    print(patient.name)
+    print(patient.allergies)
+
+patient_info = {'name': 'sarthak', 'age': 30, 'weight' : 56.5, 'married' : True, 'allergies': ['dust', 'pollen'], 'contact_details': {'email':'abc@gmail.com','Phone-number' :'123456'}}
+
+
+patient_1 = Patient(**patient_info)
+insert_patient_data(patient_1)
