5eb45c05a4bdec9e4421b4a201c391a37456c4ff
Sarthak Gulati <gulatisarthak2003@gmail.com>
2026-02-27 10:26:03 +0530

    Commit

---
+++ 02_langchain/01_Components/04_Messages/00_messages.py
@@ -0,0 +1,20 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat_model = ChatGoogleGenerativeAI(
+    model = "models/gemini-2.5-flash",
+    temperature = 0.5
+)
+
+messages = [
+    SystemMessage(content="You are a helpful Assistant"),
+    HumanMessage(content="Tell me about langchain and langraph")
+]
+
+Result = chat_model.invoke(messages)
+
+messages.append(AIMessage(content=Result.content))
+print(messages)
