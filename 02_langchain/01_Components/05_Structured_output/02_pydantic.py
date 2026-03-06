5ecf063302df9b7c64501993718551ae4f757582
Sarthak Gulati <gulatisarthak2003@gmail.com>
2026-03-06 16:36:42 +0530

    commit

---
+++ 02_langchain/01_Components/05_Structured_output/02_pydantic.py
@@ -0,0 +1,7 @@
+# Raw Data (JSON / dict / API input)
+#         ↓
+# Pydantic Model
+#         ↓
+# Validation + Parsing
+#         ↓
+# it gives me Structured Python Object, can be further converted int json or whatever output
