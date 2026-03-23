0ee1a8e217c76038d85b698054871f2faef407fb
Sarthak Gulati <gulatisarthak2003@gmail.com>
2026-03-23 10:27:18 +0530

    commit

--- 02_langchain/01_Components/06_output_parsers/01_strOutputParser.py
+++ 02_langchain/01_Components/06_output_parsers/01_strOutputParser.py
@@ -1,1 +1,2 @@
 # output parsers are the one's which take string from LLM's as a raw text and convert them into structured python objects 
+ 
---
+++ 02_langchain/01_Components/06_output_parsers/02_structuredOutputParsers.py
@@ -0,0 +1,10 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+chat_model = ChatGoogleGenerativeAI(
+    model='models/gemini-2.5-flash',
+    temperature=0.5
+)
