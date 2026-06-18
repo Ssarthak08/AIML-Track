6958fa8c3da01e1833f2baa70622657e6626ba10
Sarthak Gulati <gulatisarthak2003@gmail.com>
2026-06-18 23:56:22 +0530

    Commit

--- 05_FastAPI/main.py
+++ 05_FastAPI/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -6,7 +6,7 @@ app = FastAPI()
 def load_data():
     with open('patients.json', 'r') as f:
         data = json.load(f)
-    return data
+    return data 
 
 @app.get("/")
 def hello():
@@ -27,3 +27,20 @@ def view_patient(patient_id: str = Path(..., description="The ID of the patient 
         return data[patient_id]
     else:
         raise HTTPException(status_code=404, detail="Patient not found")
+    
+@app.get('/sort')
+def sort_patients(sort_by :str = Query(..., description="The field to sort patients by (e.g., age, bmi)"), order : str = Query("asc", description="The order to sort patients by (e.g., asc, desc)")):
+    valid_fields = ['age', 'bmi', 'weight', 'height']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f"Invalid sort field. Valid fields are: {', '.join(valid_fields)}")
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail="Invalid sort order. Valid orders are: asc, desc")
+    data = load_data()
+    sorted_patients = sorted(data.items(), key=lambda x: x[1][sort_by], reverse=(order == 'desc'))
+    return {patient_id: patient_info for patient_id, patient_info in sorted_patients}
+    
+    # query parameters are used to filter the patients based on specific criteria. For example, you can filter patients
+    # path parameters are used to retrieve specific patient information based on their unique ID. and are in the urls itself 
+    #  query parameters are after ? 
